f6bd579401baf844adfbca1d2f75be2e0bd6c789
Anamika1513 <mishraanamika8146@gmail.com>
2026-01-21 23:20:09 +0530

    21/01/26

---
+++ 21 jan 2026/fullPyramidExclamation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout<<"Enter the no of rows : ";
+    cin>>n;
+
+    for(int i = 1 ; i<=n ; i++){
+        for (int j = n-1 ; j>=i ; j--){
+            cout <<"  ";
+        }
+        for(int k = 1 ;k<=2*i-1;k++){
+            if(k%2==1){
+                cout<<"* ";
+            }
+            else{
+                cout<<"! ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ 21 jan 2026/inverseFullAllignedPyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout<<"Enter the no of rows : ";
+    cin>>n;
+
+    for(int i = 1 ; i<=n ; i++){
+        for(int k = 1 ;k<=i-1;k++){
+            cout<<" ";
+        }
+        for (int j = n ; j>=i ; j--){
+            cout <<"* ";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}
